df8c79dd670cb1c8b1acba9ef0050b9d2f1df635
KinoZero <nminhquan143@gmail.com>
2024-04-17 23:31:01 +0700

    *add Constants script

---
+++ Assets/Script/Constants.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public const string PlayerTag = "Player";
+    public const string EnemyTag = "Enemy";
+}
